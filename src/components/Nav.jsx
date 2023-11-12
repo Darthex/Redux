7d6502e379f96564f7e4b7f71760138241de94d6
Darthex <mrharshulsharma@gmail.com>
2023-11-13 04:51:02 +0530

    changed name

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Nav({ changeUsername }) {
-    const newName = "Sharma"
+    const newName = "Sharm"
     return(
         <div>
             <button onClick={() => changeUsername(newName)}>
